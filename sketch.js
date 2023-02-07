607421e574ba82b2f2d456717e81764452153b55
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-06 19:33:15 -0500

    Update sketch.js
    slowly getting zebra code and combining with the random shapes example continued

--- sketch.js
+++ sketch.js
@@ -9,10 +9,14 @@ function draw(){
   let gridSize = 80;
   size = .5;
   for (let row = gridSize; row <= width - gridSize; row += gridSize) {
-    size = random(0.3,.5);
+    //size = random(0.3,.5);
     for (let col = gridSize; col <= height - gridSize; col += gridSize) {
-      size = random(0.3,.5);
-      blendIt(row,col,size);
+      //size = random(0.3,.5);
+      var thisSize = size * (0.2 + 2 * noise(row + 83.5, col + 0.5));
+      var c = color(255, 255 * noise(row + 295, col), 0);
+      var turnDir = (noise(row,col) > 0.5);
+      //drawShapes(row, col, thisSize, c, turnDir);
+      blendIt(row,col,thisSize,c,turnDir);
       
 
       
@@ -22,9 +26,10 @@ function draw(){
 
 function blendIt(row,col,size){
   zebra(row, col, size);
-    push();
+/*    push();
       blendMode(LIGHTEST);
       fill( random(0,225), random(0,225), random(0,225), 200);
       rect(row,col,50,50);
     pop();
+*/
 }
