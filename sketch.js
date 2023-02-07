1a64b3f0932ec927a7a5b0bd183f330fb9a83a5e
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-06 19:07:23 -0500

    Create sketch.js
    Struggling to figure out how to import another .js into one file

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//I can't figure out how to import other .js files
+//I tried this tutorial https://www.youtube.com/watch?v=Yk18ZKvXBj4 but no luck getting to to work because I couldn't see what was happening on line 18
+//this seemed sorta helpful https://editor.p5js.org/kubiak200/sketches/l3cW-xhiI
+/*var zebras = [];
+
+function setup() {
+  createCanvas(400, 400);
+  for (var i = 0; i <10; i++){
+    var x = random(width);
+    var y = random(height);
+    zebras.push(new zebra(x,y));
+  }
+
+}
+
+
+function draw() {
+  background (220);
+  for (var i = 0; i zebras.length; i++) {
+    zebras[i].move();
+    zebras[i].display();
+    
+  }
+  zebra();
+  
+}
