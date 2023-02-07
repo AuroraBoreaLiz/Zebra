5e4d5af55b2fa914b2497fe9f14c485f9dac1dd1
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-06 19:30:46 -0500

    Update sketch.js
    Starting to combine the code with our animated in class example. This version has issues with rapid flickering zebras because they are inside the draw

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 function setup() {
   createCanvas(400, 400);
+}
+  
+function draw(){
   background(220);
   noStroke();
   rectMode(CENTER);
   let gridSize = 80;
   size = .5;
-  
-
   for (let row = gridSize; row <= width - gridSize; row += gridSize) {
     size = random(0.3,.5);
     for (let col = gridSize; col <= height - gridSize; col += gridSize) {
