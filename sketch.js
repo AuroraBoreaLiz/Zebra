932e5045aba68bc206860e64f166924657862d9a
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-06 19:13:55 -0500

    Update sketch.js
    Trying to get the code working with class setup. The zebra is improperly offset.

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
-var n=8;
-
 function setup(){
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(400,400);
   background(220);
-  for (var i=0; i < 5; i++) {
-  push();
-    translate(windowWidth/2, i*300);
-    drawZebra();
-  pop();
+  scale(0.5);
+  var size = 0.5 * (width/n);
+  for(var i = 0; i < n; i++) {
+    for(var j = 0; j < n; j++) {
+      var x = (0.5 + i) * (2 * size);
+      var y = (0.5 + j) * (2 * size);
+      drawZebra(x, y, size);
+    }
   }
 }
 
-function drawZebra(){
+function drawZebra(x,y,size){
   zebra();
 }
 
