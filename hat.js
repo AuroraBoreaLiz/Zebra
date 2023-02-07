6218b5a08e43b901b5bed04852589b7ac3083599
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-06 19:09:35 -0500

    Create hat.js
    making a red, pointy hat

---
+++ hat.js
@@ -0,0 +1,10 @@
+function hat(x,y,size) {
+  //translate(x,y)
+  //scale(size);
+  translate(-125,-135);
+  angleMode(DEGREES);
+  noStroke();
+  fill('#FF4538'); 
+  ellipse(124,95,60,20);
+  triangle(124, 45, 147, 97, 109, 89); 
+}
