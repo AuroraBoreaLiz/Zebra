07fd87c321a00e562a718b6e564e3d7e88f44b45
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-06 19:58:19 -0500

    Create sunglasses.js

---
+++ sunglasses.js
@@ -0,0 +1,11 @@
+function sunglasses(x,y,size) {
+  //translate(x,y)
+  //scale(size);
+  translate(-125,-135);
+  angleMode(DEGREES);
+  noStroke();
+  fill(0); 
+  ellipse(110,105,10,20);
+  rect(120,105,25,3);
+
+}
