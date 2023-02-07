b489e141377382a3430d44f6c5833b123417959c
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-06 19:22:24 -0500

    Update sketch.js
    Adding rectangles on top to play with color.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 function setup() {
   createCanvas(400, 400);
   background(220);
+  noStroke();
+  rectMode(CENTER);
   let gridSize = 80;
   size = .5;
   
@@ -9,7 +11,12 @@ function setup() {
     size = random(0.3,.5);
     for (let col = gridSize; col <= height - gridSize; col += gridSize) {
       size = random(0.3,.5);
+      push()
+      fill( random(0,255), random(0,255), random(0,255) );
+      rect(row,col,50,50);
+      pop()
       zebra(row, col, size);
+
       
     }
   }
