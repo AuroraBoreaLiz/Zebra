a3cb47d6be47b5ff95a658972dd65da164d02054
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-06 19:57:50 -0500

    Create hats2.js
    Used to be a top hat but just switched to a different color for hats because I wasn't sure how to not have the hats overlap accidentally.

---
+++ hats2.js
@@ -0,0 +1,10 @@
+function hat2(x,y,size) {
+  //translate(x,y)
+  //scale(size);
+  translate(-125,-135);
+  angleMode(DEGREES);
+  noStroke();
+  fill(0); 
+  ellipse(124,95,60,20);
+  triangle(124, 45, 147, 97, 109, 89); 
+}
