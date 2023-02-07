a679d4f9e3d7f21a71fa765027b4c6f5b81a5d58
AuroraBoreaLiz <lizmcquaidmartin@gmail.com>
2023-02-06 19:47:59 -0500

    Update sketch.js
    Working in a slider

--- sketch.js
+++ sketch.js
@@ -1,5 +1,8 @@
 function setup() {
   createCanvas(400, 400);
+  createP("Some Hats?");
+  hat_checkbox = createCheckbox("Hats", false);
+
 }
   
 function draw(){
@@ -7,17 +10,21 @@ function draw(){
   noStroke();
   rectMode(CENTER);
   let gridSize = 80;
+  let xoff = 0.0;
   size = .5;
+   
   for (let row = gridSize; row <= width - gridSize; row += gridSize) {
     size = random(0.3,.5);
     for (let col = gridSize; col <= height - gridSize; col += gridSize) {
       size = random(0.3,.5);
+      xoff = xoff + 0.01;
+      let n = noise(xoff)*width
       var thisSize = size * (0.2 + 2 * noise(row + 3, col + 0.5));
       var c = color(noise(row,col), 225 * noise(row + 2, col), 225,200);
       var turnDir = (noise(row,col) > 0.5);
       var maybeHat = (noise(row,col) > 0.5);
       zebraTime(row,col,thisSize,c,turnDir);
-      squareTime(row,col,thisSize,c,turnDir);
+      squareTime(row,col,thisSize,c,turnDir,n);
       hatTime(row,col,thisSize,c,turnDir,maybeHat);
       
     }
@@ -36,7 +43,7 @@ function zebraTime(row,col,size,color,turnDir){
 
 }
 
-function squareTime(row,col,size,color,turnDir){
+function squareTime(row,col,size,color,turnDir,n){
   push();
     translate(row,col);
     var r = frameCount * 0.05;
@@ -51,7 +58,8 @@ function squareTime(row,col,size,color,turnDir){
 
 
 function hatTime(row,col,size,color,turnDir,maybeHat){
-  push();
+  if (hat_checkbox.checked()) {
+      push();
     translate(row,col);
     var r = frameCount * 0.05;
     if(turnDir) { 
@@ -67,5 +75,8 @@ function hatTime(row,col,size,color,turnDir,maybeHat){
       hat(); 
     }
   pop();
+
+  }
+  
 
 }
